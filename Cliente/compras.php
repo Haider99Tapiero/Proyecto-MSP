26332958ebeb9f2959140496761269ab21ec715c
haider tapiero <htapierorobayo@misena.edu.co>
2018-11-06 17:01:48 -0500

    Remplazo de consulta
    Se remplazo la consulta para insertar el dato en la tabla compras

--- Cliente/compras.php
+++ Cliente/compras.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 include './conexion.php';
+$idCliente = $_SESSION["idcliente"];
 $arreglo=$_SESSION['carrito'];
 $numeroventa=0;
 $re=mysqli_query($conexion,"SELECT * FROM compras order by numeroventa DESC limit 1") or die (mysql_error());
@@ -17,15 +18,17 @@ $re=mysqli_query($conexion,"SELECT * FROM compras order by numeroventa DESC limi
   $numeroventa=$numeroventa+1;
  }
  for ($i=0; $i <count($arreglo); $i++) { 
-   mysqli_query($conexion,"INSERT INTO compras (numeroventa, imagen, nombre, precio, cantidad, subtotal) VALUES (
+   mysqli_query($conexion,"INSERT INTO compras (numeroventa, imagen, nombre, precio, cantidad, subtotal, idcliente) VALUES (
     ".$numeroventa.",
     '".$arreglo[$i]['Imagen']."',
     '".$arreglo[$i]['Nombre']."',
     '".$arreglo[$i]['Precio']."',
     '".$arreglo[$i]['Cantidad']."',
-    '".($arreglo[$i]['Precio']*$arreglo[$i]['Cantidad'])."'
+    '".($arreglo[$i]['Precio']*$arreglo[$i]['Cantidad'])."',
+    ".$idCliente."
     )")or die(mysql_error());
  }
+ $_SESSION["kkk"] = "";
  unset($_SESSION['carrito']);
- header("Location: ./admin.php");
+ header("Location: ../admin.php");
 ?>

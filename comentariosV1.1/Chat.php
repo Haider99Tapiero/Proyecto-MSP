1e7794a7531b09db2563d1d3be7ac2d21806b0cb
jhonatan1100 <jalvarezlaiton@misena.edu.co>
2018-11-22 14:40:19 -0500

    Comentarios del restaurante
    agregue la carpeta de comentarios para fucionarlos con el index

---
+++ comentariosV1.1/Chat.php
@@ -0,0 +1,89 @@
+<?php
+include"db.php";
+?>
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<!-- Bbootstrap css -->
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		
+		<script type="text/javascript">
+			function ajax(){
+				var req =new XMLHttpRequest();
+				
+				req.onreadystatechange=function(){
+					if(req.readyState==4 && req.status==200){
+						document.getElementById('chat').innerHTML=req.responseText;
+					}
+				}
+				
+				/*Hace que se recargue la pagina cada segundo*/
+				req.open('GET','observacion.php',true);
+				req.send();
+			}
+			
+			setInterval(function(){ajax();},1000);
+		</script>
+		
+		<title></title>
+	</head>
+	<body onload="ajax();">
+			<div class="container">
+			<form action="chat.php" method="post" class="">
+				<div class="form-group"></div>
+					<div class="row">
+					
+						
+						<div class="col-11 ">
+						<input name="comentario" class="form-control"  placeholder="mensaje">
+						</div>
+						<div class="col-1">
+						<input  class="btn btn-primary" type="submit" name="enviar" value="enviar">
+						</div>
+						
+					</div>
+				 </form>
+				 
+				 
+				<div id="contenedor" >
+					<div id="caja-chat">
+						<div id="chat">		
+															
+						</div>
+					</div>
+				</div>						
+				<span></span>
+				
+			</div>				 
+	
+	 
+	 
+	  <?php
+			if(isset($_POST['enviar']))
+			  {
+					$mensaje=$_POST['mensaje'];
+					$idcliente = $_SESSION;
+					$comentario=$_POST['comentario'];
+													
+						$consulta ="INSERT INTO observacion( comentario) VALUES ('$mensaje','$comentario')";
+						$ejecutar =$conexion->query($consulta);
+													
+													
+				}
+			?>
+				
+						
+    			
+   			
+	   			
+	   
+	   <!--  Scripsts Bootstrap -->
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+		integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" 
+		integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+		<script src="js/bootstrap.min.js"></script>
+	</body>
+</html>
---
+++ comentariosV1.1/observacion.php
@@ -0,0 +1,26 @@
+<?php	
+include("db.php");
+
+$consulta="SELECT comentario, cliente.nombre FROM observacion INNER JOIN cliente on observacion.cliente_idcliente = cliente.idcliente ";
+$ejecutar=$conexion-> query($consulta);
+															
+while($fila=$ejecutar->fetch_array()):
+
+
+
+		
+		 	
+			 
+			    echo"<div class=''>";
+				echo  $fila['nombre'];
+				echo"</div>";
+				
+		
+		
+			
+			echo"<div class='alert alert-primary'>";
+			echo $fila['comentario'];
+			echo"</div>";
+			
+endwhile;  
+?>

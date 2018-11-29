a3ad1202ef9a0ea64b8f4739fa59f7930af6ce44
jhonatan1100 <jalvarezlaiton@misena.edu.co>
2018-11-29 17:38:18 -0500

    diseño carritocompras
    modifique botones y estilo

--- Cliente/carritodecompras.php
+++ Cliente/carritodecompras.php
@@ -64,9 +64,20 @@
 <head>
  <meta charset="utf-8"/>
  <title>Carrito de Compras</title>
- <link rel="stylesheet" type="text/css" href="./css/estilos.css">
+ <!--<link rel="stylesheet" type="text/css" href="./css/estilos.css">-->
  <script type="text/javascript" src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
  <script type="text/javascript"  src="./js/scripts.js"></script>
+ <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
+    <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
+    <meta http-equiv="Expires" content="0">
+    <meta http-equiv="Last-Modified" content="0">
+    <meta http-equiv="Cache-Control" content="no-cache, mustrevalidate">
+    <meta http-equiv="Pragma" content="no-cache">
 </head>
 <body>
  <header>
@@ -75,6 +86,18 @@
    <img src="./imagenes/carrito.png">
   </a>
  </header> 
+ <div class="container">
+ <table class="table table-striped">
+ <thead>
+     <tr>
+     <th>Producto img</th>
+     <th>Nombre</th>
+     <th>Precio</th>
+     <th>Cantidad</th>
+     <th>Subtotal</th>
+     <th>Accion</th>
+     </tr>
+ </thead>
  <section>
   <?php
    $total=0;
@@ -84,38 +107,43 @@
    $total=0;
    for($i=0;$i<count($datos);$i++){
  ?>
-    <div class="producto">
-     <center>
-      <img src="./productos/<?php echo $datos[$i]['Imagen'];?>"><br>
-      <span><?php echo $datos[$i]['Nombre'];?></span><br>
-      <span>Precio: <?php echo $datos[$i]['Precio'];?></span><br>
-      <span>Cantidad 
+    
+     <tbody>
+      <tr>
+      <td><img src="./productos/<?php echo $datos[$i]['Imagen'];?>"></td>
+      <td><span><?php echo $datos[$i]['Nombre'];?></span></td>
+      <td><span> $<?php echo $datos[$i]['Precio'];?></span></td>
+      <td><span>
       		<input type="text" value="<?php echo $datos[$i]['Cantidad'];?>"
       		data-precio="<?php echo $datos[$i]['Precio'];?>"
       		data-id="<?php echo $datos[$i]['Id'];?>"
       		class="cantidad"
       		>
-      </span><br>
-      <span>Subtotal:<?php echo $datos[$i]['Cantidad']*$datos[$i]['Precio'];?></span><br>
-      <a href="" class="eliminar" data-id="<?php echo $datos[$i]['Id']?>" >Eliminar</a>
+      </span></td>
+      <td><span>$<?php echo $datos[$i]['Cantidad']*$datos[$i]['Precio'];?></span></td>
+          <td><a href="" class="eliminar btn btn-danger" data-id="<?php echo $datos[$i]['Id']?>" ><i class="fas fa-trash-alt"></i></a><a class="btn btn-success" href="./compras.php" class="aceptar"><i class="fas fa-cart-arrow-down"></i></a></td>
+          </tr>
+          </tbody>
 
-     </center>
-    </div>
+     
+    
   <?php
    $total=($datos[$i]['Cantidad']*$datos[$i]['Precio'])+$total;
   }
    }else{
-    echo '<center><h2>No has añadido ningun producto</h2></center>';
+    echo '<h2>No has añadido ningun producto</h2>';
    }
 
-   echo '<center><h2  id="total">Total: '.$total.'</h2></center>';
+   echo '<tbody><tr><th id="total">Total a pagar</td><th colspan="3"></th><td> $'.$total.'</td><td></td></tr></tbody>';
    if($total!=0)
    {
-    echo '<center><a href="./compras.php" class="aceptar">Comprar</a></center>';
+    echo '<thead><tr><th><a href="./compras.php" class="aceptar">Comprar</a></th><th><a href=Carrito_compras.php>Ver catalogo</a></th><th colspan="4"></th></tr><</thead>';
    }
 
   ?> 
-  <center><a href="Carrito_compras.php">Ver catalogo</a></center>
+  
  </section>
+ </table>
+ </div>
 </body>
 </html>﻿

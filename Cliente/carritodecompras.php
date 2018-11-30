78118069401c43eedf697b957e459b562a69f86d
jhonatan1100 <jalvarezlaiton@misena.edu.co>
2018-11-30 17:24:32 -0500

    agregue diseño y implemetar imagenes

--- Cliente/carritodecompras.php
+++ Cliente/carritodecompras.php
@@ -64,39 +64,16 @@
 <head>
  <meta charset="utf-8"/>
  <title>Carrito de Compras</title>
- <!--<link rel="stylesheet" type="text/css" href="./css/estilos.css">-->
+ 
  <script type="text/javascript" src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
  <script type="text/javascript"  src="./js/scripts.js"></script>
- <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet">
-    <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
-    <meta http-equiv="Expires" content="0">
-    <meta http-equiv="Last-Modified" content="0">
-    <meta http-equiv="Cache-Control" content="no-cache, mustrevalidate">
-    <meta http-equiv="Pragma" content="no-cache">
 </head>
 <body>
  <header>
-  <h1>Carrito de Compras</h1>
-  <a href="./carritodecompras.php" title="ver carrito de compras">
-   <img src="./imagenes/carrito.png">
-  </a>
- </header> 
- <div class="container">
- <table class="table table-striped">
- <thead>
-     <tr>
-     <th>Nombre</th>
-     <th>Precio</th>
-     <th>Cantidad</th>
-     <th>Subtotal</th>
-     <th>Accion</th>
-     </tr>
- </thead>
+  <?php
+     include('../menu-msp.php');
+     ?>
+ </header>
  <section>
   <?php
    $total=0;
@@ -106,42 +83,38 @@
    $total=0;
    for($i=0;$i<count($datos);$i++){
  ?>
-    
-     <tbody>
-      <tr>
-      <td><span><?php echo $datos[$i]['Nombre'];?></span></td>
-      <td><span> $<?php echo $datos[$i]['Precio'];?></span></td>
-      <td><span>
+    <div class="producto">
+     <center>
+      <img src="./productos/<?php echo $datos[$i]['Imagen'];?>"><br>
+      <span><?php echo $datos[$i]['Nombre'];?></span><br>
+      <span>Precio: <?php echo $datos[$i]['Precio'];?></span><br>
+      <span>Cantidad 
       		<input type="text" value="<?php echo $datos[$i]['Cantidad'];?>"
       		data-precio="<?php echo $datos[$i]['Precio'];?>"
       		data-id="<?php echo $datos[$i]['Id'];?>"
       		class="cantidad"
       		>
-      </span></td>
-      <td><span>$<?php echo $datos[$i]['Cantidad']*$datos[$i]['Precio'];?></span></td>
-          <td><a href="" class="eliminar btn btn-danger" data-id="<?php echo $datos[$i]['Id']?>" ><i class="fas fa-trash-alt"></i></a></td>
-          </tr>
-          </tbody>
+      </span><br>
+      <span>Subtotal:<?php echo $datos[$i]['Cantidad']*$datos[$i]['Precio'];?></span><br>
+      <a href="" class="eliminar" data-id="<?php echo $datos[$i]['Id']?>" >Eliminar</a>
 
-     
-    
+     </center>
+    </div>
   <?php
    $total=($datos[$i]['Cantidad']*$datos[$i]['Precio'])+$total;
   }
    }else{
-    echo '<h2>No has añadido ningun producto</h2>';
+    echo '<center><h2>No has añadido ningun producto</h2></center>';
    }
 
-   echo '<tbody><tr><th id="total">Total a pagar</td><th colspan="2"></th><td> $'.$total.'</td><td></td></tr></tbody>';
+   echo '<center><h2  id="total">Total: '.$total.'</h2></center>';
    if($total!=0)
    {
-    echo '<thead><tr><th><a class="btn btn-success" href="./compras.php" class="aceptar"><i class="fas fa-cart-arrow-down"></i></a></th><th><a href=Carrito_compras.php>Ver catalogo</a></th><th colspan="4"></th></tr></thead>';
+    echo '<center><a href="./compras.php" class="aceptar">Comprar</a></center>';
    }
 
   ?> 
-  
+  <center><a href="Carrito_compras.php">Ver catalogo</a></center>
  </section>
- </table>
- </div>
 </body>
 </html>﻿
--- menu-msp.php
+++ menu-msp.php
@@ -2,7 +2,6 @@
 <html lang="es">
 <head>
 	<meta charset="utf-8"/>
-	<title>Carrito de Compras</title>
 	<script type="text/javascript"  href="./js/scripts.js"></script>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -17,46 +16,31 @@
     <meta http-equiv="Pragma" content="no-cache">
 </head>
 <body>
-	<header>
-    <?php
-     include("../menu-msp.php");
-     ?>
-	</header>
-	<div class="form-group"></div>
-	<section>
-    <div class="container">
-        <table class="table table-striped">
-            <thead><tr><th>Producto</th><th>Nombre</th><th>Descripcion</th><th>Accion</th></tr></thead>
-            
-	<?php
-  include ("conexion.php");
-     $consulta="select * from plato";
- 	 $query=mysqli_query($conexion, $consulta); 
-  		while ($f=mysqli_fetch_array($query,MYSQLI_ASSOC)) {
-            
-  	?>
-    
-  
-   	
-    	<tbody><tr>
-        <td><img class="rounded float-left" src="./productos/<?php echo $f['imagen'];?>" height="45" ></td>
-        <td><span><?php echo $f['nombre'];?></span><br></td>
-        <td><a class="btn btn-info" href="./detalle.php?id=<?php echo $f['idPlato'];?>">ver</a></td>
-        <td><a class="btn btn-warning"  href="./carritodecompras.php?id=<?php  echo $f['idPlato'];?>"><i class="fas fa-cart-plus"></i></a></td>
-        </tr></tbody>
-    	
-    	
-		
-   		
-  	
- 	<?php
- 	 }
- 	?>
-            
-        </table>
-    </div>
-		
-	</section>
+
+		<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#"  style="margin-left:20px;"> <img class="logo" src="" height="40"> LOGO</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar1" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbar1">
+            <ul class="navbar-nav ml-auto"> 
+                <li class="nav-item active">
+                    <a class="nav-link" href="http://bootstrap-ecommerce.com">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item"><a class="nav-link" href="html-components.html"> Documentation </a></li>
+                <li class="nav-item dropdown b">
+                        <a style="margin-right:20px; color:#000;"  class="dropdown-toggle btn ml-2 btn-warning " href="#" data-toggle="dropdown">  Dropdown  </a>
+                    <ul class="dropdown-menu" >
+                          <li><a class="dropdown-item" href="#">Configuracion</a></li>
+                          <li><a class="dropdown-item" href="#"> Salir </a></li>
+                    </ul>
+                </li>
+        <!--<li class="nav-item">
+        <a class="btn ml-2 btn-warning" href="http://bootstrap-ecommerce.com">Download</a></li>-->
+            </ul>
+      </div>
+</nav>
+
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
